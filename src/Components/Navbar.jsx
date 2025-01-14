7dc522ddc8662cc3429e66f9fa4ef055e938b8bc
Jimena <jimenaespinoza@gmail.com>
2025-01-13 21:46:33 -0300

    Navbar was presenting problems with display flex

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -124,7 +124,7 @@ function Navbar() {
                 textDecoration: "none",
                 }}
             >
-                LOGO
+                RESCATE ANIMAL
             </Typography>
             <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
                 {pages.map((page) => (
